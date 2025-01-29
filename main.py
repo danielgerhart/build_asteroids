5e7484083825b23c9d476bd9f735dd70d296b123
danielgerhart <djgerhart@outlook.com>
2025-01-29 15:33:01 -0500

    setup player and movement abilities

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        # Fill the screen with black
-        screen.fill((0, 0, 0))  # RGB for black
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        # Refresh the screen
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
